46ac4af74fb5aad30f449cb774c2b3b88ff3f7eb
Josimar M. <madrigalg26@gmail.com>
2026-08-27 22:41:48 -0600

    v2.0.0: refactor a Rects (WIP) + TODO pendiente

--- tows_of_hanoi.py
+++ tows_of_hanoi.py
@@ -1,6 +1,6 @@
 import pygame as py
 from sys import exit
-from colorama import init, Fore, Back, Style
+from colorama import init, Fore, Style
 
 class Colors:
     BLUE = (0, 85, 255)
@@ -19,7 +19,7 @@ print(fr"""{Fore.BLUE}
   / /  | (_) |\ V  V /|  __/| |   \__ \ | (_) ||  _| / __  /| (_| || | | || (_) || |
   \/    \___/  \_/\_/  \___||_|   |___/  \___/ |_|   \/ /_/  \__,_||_| |_| \___/ |_|
 -------------------------------------------------------------------------------------
-Version: 1.9.0
+Version: 2.0.0
 By: Jasiel{Fore.RESET}
 
 Start by choosing a {Fore.BLUE}number of disks{Fore.RESET}, between {Style.BRIGHT}1{Style.RESET_ALL} and {Style.BRIGHT}20{Style.RESET_ALL}.""")
@@ -34,73 +34,93 @@ except:
 py.init()
 
 # Screen dimensions
-SCR_WIDTH = 900
-SCR_HEIGHT = 800
+SCR_WIDTH = 800
+SCR_HEIGHT = 700
 
 # Screen display settings
 screen = py.display.set_mode((SCR_WIDTH, SCR_HEIGHT))
-
 py.display.set_caption('Towers of Hanoi')
+
+# total of disks
+DISK_TOTAL = total or 5
+MIN_MOVES = 2 ** DISK_TOTAL - 1
+
+# Tower, disk and reset parameters
+TOWER_HEIGHT = int(SCR_HEIGHT // (700 / 350)) # 700: original screen height, 350: original rect height
+
+DISK_SIZE = TOWER_HEIGHT // (DISK_TOTAL + 3)
+TOWER_WIDTH = DISK_SIZE // 2
+
+DISK_RADIUS = int(DISK_SIZE // (43 / 7)) # 43: original disk size, 7: original corner radius
+BORDER_WIDTH = DISK_SIZE // 10 + 3
+
+RESET_WIDTH = int(SCR_WIDTH // (800 / 110)) # 800: original screen width, 110: original rect width
+RESET_HEIGHT = int(SCR_HEIGHT // (700 / 60)) # 700: original screen height, 60: original rect height
+
+RESET_OFFSET = 10
+RESET_X = SCR_WIDTH - RESET_WIDTH - RESET_OFFSET
+RESET_RECT = py.Rect(RESET_X, RESET_OFFSET, RESET_WIDTH, RESET_HEIGHT)
+
+# positioning and calculating towers
+MOUSE_OFFSET = 90
+TOWER_OFFSET = 50
+
+TOWER_CENTER = (SCR_WIDTH - TOWER_WIDTH) // 2
+TOWER_BASE = SCR_HEIGHT - TOWER_HEIGHT
+
+TOWER1_X = TOWER_CENTER // 2 - TOWER_OFFSET
+TOWER3_X = TOWER_CENTER * 1.5 + TOWER_OFFSET
+
+TOWER_POSITIONS = list(py.Rect(x, TOWER_BASE, TOWER_WIDTH, TOWER_HEIGHT) for x in [TOWER1_X, TOWER_CENTER, TOWER3_X])
+TRIGGER_POSITIONS = list(rect.copy().inflate(MOUSE_OFFSET * 2, 0) for rect in TOWER_POSITIONS)
 
 # text and font management
 MAIN_FONT = py.font.SysFont('comic sans', 32, True)
 RESET_FONT = py.font.SysFont('snap itc', 25)
 
-# total of disks
-DISK_TOTAL = total or 5
-
-MIN_MOVES = 2 ** DISK_TOTAL - 1
-
-# Tower, disk and reset parameters
-TOWER_HEIGHT = 350
-DISK_SIZE = TOWER_HEIGHT // (DISK_TOTAL + 3)
-TOWER_WIDTH = DISK_SIZE // 2
-
-DISK_MARGIN = DISK_SIZE // 10 + 3
-
-RESET_WIDTH = 110
-RESET_HEIGHT = 60
-RESET_OFFSET = 10
-RESET_X = SCR_WIDTH - RESET_WIDTH - RESET_OFFSET
-
-# positioning and calculating towers
-TOWER_OFFSET = 50
-
-TOWER_BASE = SCR_HEIGHT - TOWER_HEIGHT
-
-TOWER2_X = (SCR_WIDTH - TOWER_WIDTH) // 2
-TOWER1_X = TOWER2_X // 2 - TOWER_OFFSET
-TOWER3_X = TOWER2_X * 1.5 + TOWER_OFFSET
-TOWER_POSITIONS = [TOWER1_X, TOWER2_X, TOWER3_X]
-
-# Initialize disks on the first tower
 towers = [[i for i in range(DISK_TOTAL, 0, -1)],[],[]]
 placeholder = []
 
-# Game objects
+# main functions for drawing or rendering disks and text
+def render_Text(font: py.font.Font, text: str, surface_position: tuple, color: tuple = Colors.BLACK,
+		align: str = 'topleft', get_rect: bool = False, antialias: bool = True) -> py.Rect:
 
-def drawPlaceholder(stack: list, tower_position):
+	surface = font.render(text, antialias, color)
+	surface_rect = surface.get_rect()
+	setattr(surface_rect, align, surface_position)
+
+	screen.blit(surface, surface_rect)
+
+	if get_rect:
+		return surface_rect
+
+def drawPlaceholder(stack: int, tower_position: py.Rect):
 	disk_size_factor = DISK_SIZE * stack
-	disk_X = tower_position + (TOWER_WIDTH - disk_size_factor) // 2
+	disk_X = tower_position.left + (TOWER_WIDTH - disk_size_factor) // 2
 
-	disk = py.draw.rect(screen, Colors.BLUE, (disk_X, TOWER_BASE // 2, disk_size_factor, DISK_SIZE), 0, DISK_MARGIN)
-	py.draw.rect(screen, Colors.DARK_BLUE, ((disk.topleft), (disk.size)), DISK_MARGIN, DISK_MARGIN)
+	disk = py.draw.rect(screen, Colors.BLUE, (disk_X, TOWER_BASE // 2, disk_size_factor, DISK_SIZE), 0, DISK_RADIUS)
+	py.draw.rect(screen, Colors.DARK_BLUE, disk, BORDER_WIDTH, DISK_RADIUS)
 
-def drawDisks(stack: list, tower_position):
-	distance_factor = 0
+""" TODO:
+Cambiar el uso de la posicion x de la torre para calcular la distancia de los discos por un rect acomodado en el centro basado en los valores de la torre
+ajustar el tamaño en base a la torre de arriba principal para que quizas se pueda ajustar con la funcion "inflate" la cual permite aumentar el tamaño de los lados,
+	manteniendo la posicion
+"""
 
-	for i in stack:
+def drawDisks(stack: list, tower_position: py.Rect):
+	fact_values = range(DISK_SIZE, (DISK_SIZE + 1) * len(stack), DISK_SIZE + 1)
+	for i, distance_factor in zip(stack, fact_values):
 		disk_size_factor = DISK_SIZE * i
 		disk_X = tower_position + (TOWER_WIDTH - disk_size_factor) // 2
-		disk_Y = SCR_HEIGHT - DISK_SIZE - distance_factor
+		disk_Y = SCR_HEIGHT - distance_factor
 
-		disk = py.draw.rect(screen, Colors.BLUE, (disk_X, disk_Y, disk_size_factor, DISK_SIZE), 0, DISK_MARGIN)
-		py.draw.rect(screen, Colors.DARK_BLUE, ((disk.topleft), (disk.size)), DISK_MARGIN, DISK_MARGIN)
-		distance_factor += DISK_SIZE + 1
+		disk = py.draw.rect(screen, Colors.BLUE, (disk_X, disk_Y, disk_size_factor, DISK_SIZE), 0, DISK_RADIUS)
+		py.draw.rect(screen, Colors.DARK_BLUE, disk, BORDER_WIDTH, DISK_RADIUS)
 
 # Function for positioning the disks
-def takeDiskFromTower(index: int):
+def takeDiskFromTower(index: int) -> bool:
 	stack: list = towers[index]
+
 	if not stack:
 		return True
 
@@ -109,11 +129,11 @@ def takeDiskFromTower(index: int):
 	stack.pop()
 	return False
 
-def placeDiskInTower(index: int):
+def placeDiskInTower(index: int) -> tuple[bool, int]:
 	stack: list = towers[index]
 	disk, idx = placeholder
 
-	top_disk = min(stack or [0])
+	top_disk = 0 if not stack else stack[-1]
 
 	if top_disk == 0 or top_disk > disk:
 		stack.append(disk)
@@ -122,89 +142,34 @@ def placeDiskInTower(index: int):
 		return False, 0
 	return True, 0 if idx == index else 1
 
-# functions for checking on events
-MOUSE_OFFSET = 90
-
-def clickedTower(mouseX, mouseY):
-	def is_touching(tower):
-		isX = (mouseX >= tower - MOUSE_OFFSET and mouseX <= tower + TOWER_WIDTH + MOUSE_OFFSET)
-		isY = (mouseY >= TOWER_BASE and mouseY <= TOWER_BASE + TOWER_HEIGHT)
-		return isX and isY
-
-	check = lambda tower, num: (is_touching(tower) and str(num))
-	return check(TOWER1_X, 0) or check(TOWER2_X, 1) or check(TOWER3_X, 2) or None
-
-def onResetClicked(mouseX, mouseY):
-	isX = (mouseX >= RESET_X and mouseX <= RESET_X + RESET_WIDTH)
-	isY = (mouseY >= RESET_OFFSET and mouseY <= RESET_OFFSET + RESET_HEIGHT)
-	isClicking = isX and isY
-
-	if not isClicking:
-		return isTake, move_count
-
-	if any(placeholder):
-		towers[0].append(placeholder[0])
-		placeholder.clear()
-
-	if any(towers[i] for i in range(1, 3)):
-		for i in range(1, 3):
-			towers[0].extend(towers[i])
-			towers[i].clear()
-
-	towers[0].sort(reverse=True)
-	return True, 0
-
 # As the name suggests, this is the main loop function
 move_count = 0
 
 def main():
 	screen.fill(Colors.WHITE)
 
-	# Reset button background
-	py.draw.rect(screen, Colors.BLUE, (RESET_X, RESET_OFFSET, RESET_WIDTH, RESET_HEIGHT), 0, 11)
+	reset_bg = py.draw.rect(screen, Colors.BLUE, RESET_RECT, 0, 11)
 
-	# Render minimum moves text
-	min_moves_text = MAIN_FONT.render(f'Best: {MIN_MOVES}', True, Colors.BLACK)
-	min_moves_rect = min_moves_text.get_rect()
-	min_moves_rect.topleft = (20, 10)
-
-	# Render move count text
-	move_count_text = MAIN_FONT.render('Move count: ' + str(move_count), True, Colors.BLACK)
-	move_count_rect = move_count_text.get_rect()
-	move_count_rect.topleft = min_moves_rect.bottomleft
-
-	# Render reset button Text
-	reset_button = RESET_FONT.render('Reset', True, Colors.WHITE)
-	reset_rect = reset_button.get_rect()
-	reset_rect.center = (RESET_X + RESET_WIDTH // 2, RESET_OFFSET + RESET_HEIGHT // 2)
-
-	screen.blit(move_count_text, move_count_rect)
-	screen.blit(min_moves_text, min_moves_rect)
-	screen.blit(reset_button, reset_rect)
+	min_moves_text = render_Text(MAIN_FONT, f'Best: {MIN_MOVES}', (20, 10), get_rect=True)
+	render_Text(MAIN_FONT, f'Move count: {move_count}', min_moves_text.bottomleft)
+	render_Text(RESET_FONT, 'Reset', reset_bg.center, Colors.WHITE, 'center')
 
 	for i in range(3):
-		py.draw.rect(screen, Colors.GREEN, (TOWER_POSITIONS[i] - MOUSE_OFFSET, TOWER_BASE, TOWER_WIDTH + MOUSE_OFFSET * 2, TOWER_HEIGHT))
-		py.draw.rect(screen, Colors.BROWN, (TOWER_POSITIONS[i], TOWER_BASE, TOWER_WIDTH, TOWER_HEIGHT))
+		py.draw.rect(screen, Colors.GREEN, TRIGGER_POSITIONS[i])
+		py.draw.rect(screen, Colors.BROWN, TOWER_POSITIONS[i])
 
-		drawDisks(towers[i], TOWER_POSITIONS[i])
+		drawDisks(towers[i], TOWER_POSITIONS[i].left)
 
 	placeholder and drawPlaceholder(placeholder[0], TOWER_POSITIONS[placeholder[1]])
 
-	for i in range(1, 3):
-		if len(towers[i]) == DISK_TOTAL:
-			# Render solved achievement text
-			score1, score2 = "That's a perfect score!",'You achieved to solve it!'
-			solved_state = move_count == MIN_MOVES and score1 or score2
-
-			solved_text = MAIN_FONT.render(solved_state, True, Colors.BLUE)
-			solved_rect = solved_text.get_rect()
-			solved_rect.center = (SCR_WIDTH // 2, TOWER_BASE // 2.8)
-
-			screen.blit(solved_text, solved_rect)
+	if any(len(towers[i]) == DISK_TOTAL for i in range(1, 3)):
+		w1, w2 = "That's a perfect score!", 'You achieved to solve it!'
+		solved_state = move_count == MIN_MOVES and w1 or w2
+		render_Text(MAIN_FONT, solved_state, (SCR_WIDTH // 2, TOWER_BASE // 2), Colors.BLUE, 'center')
 
 	py.display.flip()
 
-isTake = True
+isHolding = True
 isRunning = True
 while isRunning:
 	for event in py.event.get():
@@ -213,16 +178,35 @@ while isRunning:
 		elif event.type == py.MOUSEBUTTONDOWN:
 			mouseX, mouseY = py.mouse.get_pos()
 
-			clicked_tower = int(clickedTower(mouseX, mouseY) or -1)
+			clicked_tower = -1
+			for i in range(3):
+				left = TOWER_POSITIONS[i].left - MOUSE_OFFSET
+				width = TOWER_WIDTH + MOUSE_OFFSET * 2
+
+				trigger = py.Rect(left, TOWER_BASE, width, TOWER_HEIGHT)
+
+				if trigger.collidepoint(mouseX, mouseY):
+					clicked_tower = i
+
 			if clicked_tower != -1:
-				if isTake:
-					isTake = takeDiskFromTower(clicked_tower)
+				if isHolding:
+					isHolding = takeDiskFromTower(clicked_tower)
 				else:
-					isTake, move = placeDiskInTower(clicked_tower)
+					isHolding, move = placeDiskInTower(clicked_tower)
 					move_count += move
-			else:
-				isTake, move_count = onResetClicked(mouseX, mouseY)
-	main( )
+			elif RESET_RECT.collidepoint(mouseX, mouseY):
+				if placeholder:
+					towers[0].append(placeholder[0])
+					placeholder.clear()
+
+				if any(towers[i] for i in range(1, 3)):
+					for i in range(1, 3):
+						towers[0].extend(towers[i])
+						towers[i].clear()
+
+				towers[0].sort(reverse=True)
+				isHolding, move_count = True, 0
+	main()
 
 py.quit()
 exit()
